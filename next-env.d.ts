0599b2a64211ca50b6720b984f4133042619ae7b
doganerdk <77238781+doganerdk@users.noreply.github.com>
2026-06-01 23:42:48 +0300

    33

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
